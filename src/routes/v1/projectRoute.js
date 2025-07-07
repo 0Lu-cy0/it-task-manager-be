697a2cb5e6abe100796df55f262df47b9041ee49
0Lu-cy0 <nc12042004@gmail.com>
2025-07-07 21:43:49 +0700

    Create validation floor

--- src/routes/v1/projectRoute.js
+++ src/routes/v1/projectRoute.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 const Router = express.Router()
+import { projectValidation } from '~/validations/projectValidation'
 
 Router.route('/')
   .get((req, res) => res.status(StatusCodes.OK).json({
     message: 'API get list projects',
   }))
-  .post((req, res) => res.status(StatusCodes.CREATED).json({
-    message: 'API create new post',
-  }))
+  .post(projectValidation.createNew)
 
-export const projectRoutes = Router
+export const projectRoute = Router
