45631adf4506862a71332b96ee33014657e347f3
0Lu-cy0 <nc12042004@gmail.com>
2025-07-07 15:34:02 +0700

    Create routing

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,14 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { projectRoutes } from './projectRoutes'
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'Tất cả các API V1 đã sẵn sàng để sử dụng',
+  })
+})
+
+Router.use('/projects', projectRoutes)
+
+export const APIs_V1 = Router
