42d1de798eec873606afe47471219fb66c5992e5
0Lu-cy0 <nc12042004@gmail.com>
2025-07-07 22:24:19 +0700

    Create Controller Handling

--- src/routes/v1/projectRoute.js
+++ src/routes/v1/projectRoute.js
@@ -2,11 +2,12 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 const Router = express.Router()
 import { projectValidation } from '~/validations/projectValidation'
+import { projectController } from '~/controllers/projectController'
 
 Router.route('/')
   .get((req, res) => res.status(StatusCodes.OK).json({
     message: 'API get list projects',
   }))
-  .post(projectValidation.createNew)
+  .post(projectValidation.createNew, projectController.createNew)
 
 export const projectRoute = Router
