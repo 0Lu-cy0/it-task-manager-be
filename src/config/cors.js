344c143b6bf778ab91e27c3ef18986ce1e8d8865
Nguyễn Văn Toàn <142965245+toankunsss@users.noreply.github.com>
2025-12-03 07:54:15 +0700

    s

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,25 +6,21 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    // Update mới: Ở video số 75 trong chuỗi MERN Stack PRO khi chúng ta deploy dự án lên một Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh nhé :D
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Cho phép Postman và môi trường dev
+    if (!origin && (env.BUILD_MODE === 'dev' || env.BUILD_MODE === 'production')) {
       return callback(null, true)
     }
 
-    // Kiểm tra dem origin có phải là domain được chấp nhận hay không
+    // Kiểm tra domain có trong whitelist không
     if (WHITELIST_DOMAINS.includes(origin)) {
       return callback(null, true)
     }
 
-    // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    // Domain không được chấp nhận
+    return callback(
+      new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`)
+    )
   },
-
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
   optionsSuccessStatus: 200,
-
-  // CORS sẽ cho phép nhận cookies từ request, (Nhá hàng :D | Ở khóa MERN Stack Advance nâng cao học trực tiếp mình sẽ hướng dẫn các bạn đính kèm jwt access token và refresh token vào httpOnly Cookies)
   credentials: true,
 }
